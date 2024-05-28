ff1d72378fe99091b44bbbfa228841186f7fa4ee
ziadhassan7 <ziadhassan7@gmail.com>
2024-05-29 00:58:08 +0300

    Profile Page(UI): Add new sections

--- lib/features/profile/presentation/screens/profile_page.dart
+++ lib/features/profile/presentation/screens/profile_page.dart
@@ -12,15 +12,68 @@ class ProfilePage extends StatelessWidget {
       appBar: CustomAppBar(context, title: LocalTxt().profile,),
 
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+
         children: [
-          Text(getUserName())
+          //Name
+          label(LocalTxt().name),
+          Text(getUserName()),
+
+          //Email
+          label(LocalTxt().email),
+          Text(getEmail()),
+
+          //Password
+          label(LocalTxt().password),
+          Text(getPassword()),
+
+          //Gender
+          label(LocalTxt().gender),
+          Text(getGender()),
+
+          //BirthDate
+          label(LocalTxt().birthDate),
+          Text(getBirthDate()),
+
+          // Open Location Button
+          openLocation()
         ],
       ),
     );
   }
 
+  /// Widgets:
+  Widget label(String title, ){
+    return Text(
+      title,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget openLocation(){
+    return ElevatedButton(
+        onPressed: (){
+
+        },
+        child: Text(LocalTxt().openLocationButton)
+    );
+  }
+
+  /// Get Data:
   String getUserName(){
     return UserData.getUserName();
+  }
+  String getEmail(){
+    return UserData.getEmail();
+  }
+  String getPassword(){
+    return UserData.getPassword();
+  }
+  String getGender(){
+    return UserData.getGender();
+  }
+  String getBirthDate(){
+    return UserData.getBirthDate();
   }
 
   String getLocation(){
