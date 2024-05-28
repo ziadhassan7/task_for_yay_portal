06a6ca87a832193d452eff298680ef308a3cf502
ziadhassan7 <ziadhassan7@gmail.com>
2024-05-29 02:13:53 +0300

    Add controller for Gender and BirthDate

--- lib/features/form/presentation/controllers/input_fields_controller.dart
+++ lib/features/form/presentation/controllers/input_fields_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_for_yay_portal/features/form/presentation/views/gender_field.dart';
 
 class InputFieldsController {
 
@@ -6,5 +7,6 @@ class InputFieldsController {
   static final TextEditingController emailController = TextEditingController();
   static final TextEditingController passwordController = TextEditingController();
 
-
+  static Gender gender = Gender.M;
+  static String birthDate = '--';
 }
