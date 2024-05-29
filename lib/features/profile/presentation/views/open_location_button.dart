49b4510568fb2fc3d590b02917dd52c12a1fe417
ziadhassan7 <ziadhassan7@gmail.com>
2024-05-29 04:39:03 +0300

    Open User's Location on Google Maps by pressing a button: customized button

---
+++ lib/features/profile/presentation/views/open_location_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../../../../core/localization/localization.dart';
+import '../../../../core/map_util.dart';
+import '../../../form/data/datasource/user_data.dart';
+
+
+class OpenLocationButton extends StatelessWidget {
+  const OpenLocationButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: (){
+          List<String> location = getLocation().split(',');
+          MapUtils.openMap(location[0], location[1]);
+        },
+        child: Text(LocalTxt().openLocationButton)
+    );
+  }
+
+  String getLocation(){
+    return UserData.getLocation();
+  }
+}
+
+
